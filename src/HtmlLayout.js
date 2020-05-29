4b512c0fa42bc165b3cf3e5f863840f9b8c954d2
kwakyongkee <kyk0185@gmail.com>
2020-05-29 16:27:20 +0900

    * 사용자 페이지 card 형식으로 레이아웃 구성

--- src/HtmlLayout.js
+++ src/HtmlLayout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { userInfo, data } from './dummy';
 import './HtmlLayout.css';
+import { Button } from 'react-bootstrap';
 
 class HtmlLayout extends Component {
     constructor(props) {
@@ -9,16 +10,16 @@ class HtmlLayout extends Component {
         this.state = {
             dummyData: data,
             role_name: '',
-            user_code: ''
+            user_code: '',
+            user_name: ''
         }
     }
-
     componentDidMount = _ => {
 
         //기관별 이름, 코드
         for (let i = 0; i < userInfo.roles.length; i++) {
             if (userInfo.roles[i].role_no === Number(this.props.match.params['role_no'])) {
-                this.setState({ role_name: userInfo.roles[i].role_name, user_code: userInfo.roles[i].code })
+                this.setState({ role_name: userInfo.roles[i].role_name, user_code: userInfo.roles[i].code ,user_name:userInfo.users[i].user_name})
             }
         }
         setInterval(this.onRefresh, 1000);
@@ -48,120 +49,77 @@ class HtmlLayout extends Component {
     }
     render() {
         const { dummyData } = this.state;
-
         return (
-            <div className="frame">
-                <div className="header" >
+            <div className="frame-user">
+                <div className="header-user" >
                     <h2>{this.state.role_name}</h2>
                     <div className="columnAlign_header">
-                        <button className="cover" onClick={this.logOut}>
-                            로그아웃
-                        </button>
+                        <h5>{this.state.user_name}님</h5>
+                        <Button variant="outline-secondary" onClick={this.logOut} style={{marginLeft:20}}>로그아웃</Button>
                     </div>
                 </div>
-                <div className="container">
-                    <div className="content1">
-                        <a href="open#" className="section1 greens"></a>
-                        <div className="section1 greens" />
-                        <div className="section1 blueviolets" />
-                        <div className="section1 blueviolets" />
-                        <div className="section1 blueviolets" />
-                    </div>
-                    <div className="content2">
-                        <div className="section2">
-                            <div className="section2_1 greens" />
-                            <div className="section2_2 blueviolets" >
-                                <div className="filebox columnAlign">
-                                    <label htmlFor="ex_file">파일 업로드</label>
-                                    <input type="file" id="ex_file" onChange={e => this.fileUpload(e)} />
+                <div className="container-user">
+                    <div className="content-user">
+                        <div className="card-layout">
+                            <h2 className="card-top">Layout</h2>
+                            <div className="card-bottom">
+                                <h4 className="card-title">Flexbox</h4>
+                                <p className="card-info">Flexbox is now the default, and Bootstrap 4 supports SASS out of the box.</p>
+                                <div className="button-layout">
+                                    <Button variant="outline-secondary" >조회</Button>
                                 </div>
                             </div>
                         </div>
-                        <div className="section2">
-                            <div className="section2_1 greens" >
-                                <h3>Title</h3>
-                            </div>
-                            <div className="section2_2 blueviolets " >
-                                <div className="columnAlign">
-                                    <a href="#open" className="cover">button</a>
-                                </div>
-                                <div id="open" className="modal">
-                                    <div className="window">
-                                        <div className="window-header">
-                                            <h2>Title</h2>
-                                        </div>
-                                        <div className="window-contents">
-                                            <p>Contents</p>
-                                            <div className="window-contents-button">
-                                                <a href="#close" className="button">확인</a>
-                                                <a href="#close" className="button line">취소</a>
-                                            </div>
-                                        </div>
-                                    </div>
+                        <div className="card-layout">
+                            <h2 className="card-top">Layout</h2>
+                            <div className="card-bottom">
+                                <h4 className="card-title">Flexbox</h4>
+                                <p className="card-info">Flexbox is now the default, and Bootstrap 4 supports SASS out of the box.</p>
+                                <div className="button-layout">
+                                    <Button variant="outline-secondary" >조회</Button>
                                 </div>
                             </div>
                         </div>
-                        <div className="section2">
-                            <div className="section2_1 blueviolets" >
-                                <h3>Title</h3>
-                            </div>
-                            <div className="section2_2 greens" >
-                                <div className="columnAlign">
-                                    <a href="#open" className="cover">button</a>
-                                </div>
-                                <div id="open" className="modal">
-                                    <div className="window">
-                                        <div className="window-header">
-                                            <h2>Title</h2>
-                                        </div>
-                                        <div className="window-contents">
-                                            <p>Contents</p>
-                                            <div className="window-contents-button">
-                                                <a href="#close" className="button">확인</a>
-                                                <a href="#close" className="button line">취소</a>
-                                            </div>
-                                        </div>
-                                    </div>
+                        <div className="card-layout">
+                            <h2 className="card-top">Layout</h2>
+                            <div className="card-bottom">
+                                <h4 className="card-title">Flexbox</h4>
+                                <p className="card-info">Flexbox is now the default, and Bootstrap 4 supports SASS out of the box.</p>
+                                <div className="button-layout">
+                                    <Button variant="outline-secondary" >조회</Button>
                                 </div>
                             </div>
                         </div>
-                        <div className="section2">
-                            <div className="section2_1 blueviolets" >
-                                <h3>Title</h3>
-                            </div>
-                            <div className="section2_2 greens" >
-                                <div className="columnAlign">
-                                    <a href="#open" className="cover">button</a>
-                                </div>
-                                <div id="open" className="modal">
-                                    <div className="window">
-                                        <div className="window-header">
-                                            <h2>Title</h2>
-                                        </div>
-                                        <div className="window-contents">
-                                            <p>Contents</p>
-                                            <div className="window-contents-button">
-                                                <a href="#close" className="button">확인</a>
-                                                <a href="#close" className="button line">취소</a>
-                                            </div>
-                                        </div>
-                                    </div>
+                        <div className="card-layout">
+                            <h2 className="card-top">Layout</h2>
+                            <div className="card-bottom">
+                                <h4 className="card-title">Flexbox</h4>
+                                <p className="card-info">Flexbox is now the default, and Bootstrap 4 supports SASS out of the box.</p>
+                                <div className="button-layout">
+                                    <Button variant="outline-secondary" >조회</Button>
                                 </div>
                             </div>
                         </div>
-                        <div className="section2">
-                            <div className="section2_1 greens" />
-                            <div className="section2_2 blueviolets" />
+                        <div className="card-layout">
+                            <h2 className="card-top">Layout</h2>
+                            <div className="card-bottom">
+                                <h4 className="card-title">Flexbox</h4>
+                                <p className="card-info">Flexbox is now the default, and Bootstrap 4 supports SASS out of the box.</p>
+                                <div className="button-layout">
+                                    <Button variant="outline-secondary" >조회</Button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     {dummyData.filter(item => item.code === this.state.user_code).map((item, index) => (
-                        <div className="content3" key={index}>
+                        <div className="content-user" key={index}>
                             {
                                 item.titles.map((title, titleIndex) => (
-                                    <div className="section3 grays" key={`title-${index}-${titleIndex}`}>
-                                        <div className="columnAlign filebox">
-                                            <h3>{title.fileName}</h3>
-                                            <label htmlFor="ex_file" style={title.fileName ? {} : { display: 'none' }}>파일 업로드</label>
+                                    <div className="card-bottom" key={`title-${index}-${titleIndex}`} style={{margin:10}}>
+                                        <h4 className="card-title">{title.fileName}</h4>
+                                        <p className="card-info">Flexbox is now the default, and Bootstrap 4 supports SASS out of the box.</p>
+                                        <div className="button-layout filebox">
+                                            <label htmlFor="ex_file" style={title.fileName ? {} : { display: 'none' }}>조회</label>
                                             <input type="file" id="ex_file" onChange={e => this.fileUpload(e)} />
                                         </div>
                                     </div>
@@ -169,15 +127,46 @@ class HtmlLayout extends Component {
                             }
                         </div>
                     ))}
-                    <div className="content4">
-                        <div className="section4 yellows" />
-                        <div className="section4 blueviolets" />
-                        <div className="section4 blueviolets" />
-                        <div className="section4 blueviolets" />
-                        <div className="section4 blueviolets" />
+                    <div className="content-user">
+                            <div className="card-bottom" style={{margin:10}}>
+                                <h4 className="card-title">Flexbox</h4>
+                                <p className="card-info">Flexbox is now the default, and Bootstrap 4 supports SASS out of the box.</p>
+                                <div className="button-layout">
+                                    <Button variant="outline-secondary" >조회</Button>
+                                </div>
+                            </div>
+                            <div className="card-bottom" style={{margin:10}}>
+                                <h4 className="card-title">Flexbox</h4>
+                                <p className="card-info">Flexbox is now the default, and Bootstrap 4 supports SASS out of the box.</p>
+                                <div className="button-layout">
+                                    <Button variant="outline-secondary" >조회</Button>
+                                </div>
+                            </div>
+                            <div className="card-bottom" style={{margin:10}}>
+                                <h4 className="card-title">Flexbox</h4>
+                                <p className="card-info">Flexbox is now the default, and Bootstrap 4 supports SASS out of the box.</p>
+                                <div className="button-layout">
+                                    <Button variant="outline-secondary" >조회</Button>
+                                </div>
+                            </div>
+                            <div className="card-bottom" style={{margin:10}}>
+                                <h4 className="card-title">Flexbox</h4>
+                                <p className="card-info">Flexbox is now the default, and Bootstrap 4 supports SASS out of the box.</p>
+                                <div className="button-layout">
+                                    <Button variant="outline-secondary" >조회</Button>
+                                </div>
+                            </div>
+                            <div className="card-bottom" style={{margin:10}}>
+                                <h4 className="card-title">Flexbox</h4>
+                                <p className="card-info">Flexbox is now the default, and Bootstrap 4 supports SASS out of the box.</p>
+                                <div className="button-layout">
+                                    <Button variant="outline-secondary">조회</Button>
+                                </div>
+                            
+                            </div>
                     </div>
                 </div>
-                <div className="footer" />
+                <div className="footer-user" />
             </div>
         );
     }
