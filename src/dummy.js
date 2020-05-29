7126e50d065824b6643802b615e2c022d0d12fce
kwakyongkee <kyk0185@gmail.com>
2020-05-29 17:58:27 +0900

    * 더미 데이터 추가

--- src/dummy.js
+++ src/dummy.js
@@ -8,6 +8,8 @@ export const data = [
             {
                 key:1,
                 name:'제목1',
+                fileName: '카카오톡 설치파일.exe',
+                info: 'Flexbox is now the default, and Bootstrap 4 supports SASS out of the box.'
             },
             {
                 key:2,
@@ -25,7 +27,7 @@ export const data = [
     },
     {
         key: 2,
-        sido:'인천시교육청',
+        sido:'인천광역시교육청',
         code:'E10',
         titles:
         [
@@ -49,15 +51,11 @@ export const data = [
                 key:5,
                 name:'제목5',
             },
-            {
-                key:6,
-                name:'제목6',
-            }
         ]
     },
     {
         key: 3,
-        sido:'부산시교육청',
+        sido:'부산광역시교육청',
         code:'C10',
         titles:
         [
@@ -83,6 +81,474 @@ export const data = [
             }
         ]
     },
+    {
+        key: 4,
+        sido:'강원도교육청',
+        code:'K10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+        ]
+    },
+    {
+        key: 5,
+        sido:'경기도교육청',
+        code:'J10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+        ]
+    },
+    {
+        key: 6,
+        sido:'경상남도교육청',
+        code:'S10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            }
+        ]
+    },
+    {
+        key: 7,
+        sido:'경상북도교육청',
+        code:'R10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+        ]
+    },
+    {
+        key: 8,
+        sido:'광주광역시교육청',
+        code:'F10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+            {
+                key:5,
+                name:'제목5',
+            }
+        ]
+    },
+    {
+        key: 9,
+        sido:'대구광역시교육청',
+        code:'D10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+            {
+                key:5,
+                name:'제목5',
+            }
+        ]
+    },
+    {
+        key: 10,
+        sido:'대전광역시교육청',
+        code:'G10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+            {
+                key:5,
+                name:'제목5',
+            }
+        ]
+    },
+    {
+        key: 11,
+        sido:'세종특별자치시교육청',
+        code:'',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+        ]
+    },
+    {
+        key: 12,
+        sido:'울산광역시교육청',
+        code:'H10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+            {
+                key:5,
+                name:'제목5',
+            }
+        ]
+    },
+    {
+        key: 13,
+        sido:'전라남도교육청',
+        code:'Q10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+            {
+                key:5,
+                name:'제목5',
+            }
+        ]
+    },
+    {
+        key: 14,
+        sido:'전라북도교육청',
+        code:'P10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+        ]
+    },
+    {
+        key: 15,
+        sido:'제주도교육청',
+        code:'T10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+            {
+                key:5,
+                name:'제목5',
+            }
+        ]
+    },
+    {
+        key: 16,
+        sido:'총괄',
+        code:'A',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+            {
+                key:5,
+                name:'제목5',
+            }
+        ]
+    },
+    {
+        key: 17,
+        sido:'충청남도교육청',
+        code:'N10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+        ]
+    },
+    {
+        key: 18,
+        sido:'충청북도교육청',
+        code:'M10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+            {
+                key:5,
+                name:'제목5',
+            }
+        ]
+    },
+    {
+        key: 19,
+        sido:'KERIS 한국교육학술정보원',
+        code:'V10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+            {
+                key:5,
+                name:'제목5',
+            }
+        ]
+    },
+    {
+        key: 20,
+        sido:'DR1센터',
+        code:'W10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+            {
+                key:5,
+                name:'제목5',
+            }
+        ]
+    },
+    {
+        key: 21,
+        sido:'DR2센터',
+        code:'X10',
+        titles:
+        [
+            {
+                key:1,
+                name:'제목1',
+            },
+            {
+                key:2,
+                name:'제목2',
+            },
+            {
+                key:3,
+                name:'제목3',
+            },
+            {
+                key:4,
+                name:'제목4',
+            },
+        ]
+    },
 ]
 export const userInfo = {
     //roles
@@ -91,12 +557,49 @@ export const userInfo = {
        {role_no : 2, role_name: '서울시교육청', code:'B10'},
        {role_no : 3, role_name: '인천광역시교육청', code:'E10'},
        {role_no : 4, role_name: '부산광역시교육청', code:'C10'},
+       {role_no : 5, role_name: '강원도교육청', code:'K10'},
+       {role_no : 6, role_name: '경기도교육청', code:'J10'},
+       {role_no : 7, role_name: '경상남도교육청', code:'S10'},
+       {role_no : 8, role_name: '경상북도교육청', code:'R10'},
+       {role_no : 9, role_name: '광주광역시교육청', code:'F00'},
+       {role_no : 10, role_name: '대구광역시교육청', code:'D10'},
+       {role_no : 11, role_name: '대전광역시교육청', code:'G10'},
+       {role_no : 12, role_name: '세종특별자치시교육청', code:''},
+       {role_no : 13, role_name: '울산광역시교육청', code:'H10'},
+       {role_no : 14, role_name: '전라남도교육청', code:'Q10'},
+       {role_no : 15, role_name: '전라북도교육청', code:'P10'},
+       {role_no : 16, role_name: '제주도교육청', code:'T10'},
+       {role_no : 17, role_name: '총괄', code:'A'},
+       {role_no : 18, role_name: '충청남도교육청', code:'N10'},
+       {role_no : 19, role_name: '충청북도교육청', code:'M10'},
+       {role_no : 20, role_name: 'KERIS 한국교육학술정보원', code:'V10'},
+       {role_no : 21, role_name: 'DR1센터', code:'W10'},
+       {role_no : 22, role_name: 'DR2센터', code:'X10'},
      ],
      //users
       users:[
        {user_no : 1, id: 'admin', pw: '1234', user_name : '홍길동', role_no : 1},
-       {user_no : 2, id: 'susers', pw: '1234', user_name : '홍길동2', role_no : 2},
-       {user_no : 3, id: 'iusers', pw: '1234', user_name : '홍길동3', role_no : 3},
-       {user_no : 4, id: 'busers', pw: '1234', user_name : '홍길동4', role_no : 4},
+       {user_no : 2, id: 'seoul', pw: '1234', user_name : '홍길동', role_no : 2},
+       {user_no : 3, id: 'incheon', pw: '1234', user_name : '홍길동', role_no : 3},
+       {user_no : 4, id: 'busan', pw: '1234', user_name : '홍길동', role_no : 4},
+       {user_no : 5, id: 'kangwon', pw: '1234', user_name : '홍길동', role_no : 5},
+       {user_no : 6, id: 'gyeonggi', pw: '1234', user_name : '홍길동', role_no : 6},
+       {user_no : 7, id: 'gyeongnam', pw: '1234', user_name : '홍길동', role_no : 7},
+       {user_no : 8, id: 'gyeongbuk', pw: '1234', user_name : '홍길동', role_no : 8},
+       {user_no : 9, id: 'gwangju', pw: '1234', user_name : '홍길동', role_no : 9},
+       {user_no : 10, id: 'daegu', pw: '1234', user_name : '홍길동', role_no : 10},
+       {user_no : 11, id: 'daegeon', pw: '1234', user_name : '홍길동', role_no : 11},
+       {user_no : 12, id: 'sejong', pw: '1234', user_name : '홍길동', role_no : 12},
+       {user_no : 13, id: 'ulsan', pw: '1234', user_name : '홍길동', role_no : 13},
+       {user_no : 14, id: 'jeonnam', pw: '1234', user_name : '홍길동', role_no : 14},
+       {user_no : 15, id: 'jeonbuk', pw: '1234', user_name : '홍길동', role_no : 15},
+       {user_no : 16, id: 'jeju', pw: '1234', user_name : '홍길동', role_no : 16},
+       {user_no : 17, id: 'general', pw: '1234', user_name : '홍길동', role_no : 17},
+       {user_no : 18, id: 'chungnam', pw: '1234', user_name : '홍길동', role_no : 18},
+       {user_no : 19, id: 'chungbuk', pw: '1234', user_name : '홍길동', role_no : 19},
+       {user_no : 20, id: 'keris', pw: '1234', user_name : '홍길동', role_no : 20},
+       {user_no : 21, id: 'dr1', pw: '1234', user_name : '홍길동', role_no : 21},
+       {user_no : 22, id: 'dr2', pw: '1234', user_name : '홍길동', role_no : 22},
+
      ]
 }
